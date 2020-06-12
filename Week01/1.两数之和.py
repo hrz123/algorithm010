b49ba3ff104d93936282ead0d2dcad67674dc021
hrz123 <2403076194@qq.com>
2020-06-12 11:00:29 +0800

    NEW: 两数之和提交

---
+++ Week01/1.两数之和.py
@@ -0,0 +1,28 @@
+# 1.两数之和.py
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if not nums or len(nums) < 2:
+            return []
+
+        memo = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            if num in memo:
+                return [memo[num], i]
+            memo[target - num] = i
+
+        return []
+
+
+def main():
+    s = Solution()
+    res = s.twoSum([2, 11, 15], 9)
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
