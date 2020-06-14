13cb044dcda0f0e3379bc90466721c89bcd1c3bd
hrz123 <2403076194@qq.com>
2020-06-14 23:55:22 +0800

    NEW: 更新接雨水题目

--- Week01/42.接雨水.py
+++ Week01/42.接雨水.py
@@ -3,6 +3,42 @@
 from typing import List
 
 
+# 暴力法
+# 直接按问题描述进行。对于数组中的每个元素，我们找出下雨后水能达到的最高位置，等于两边最大高度的较小值减去当前高度的值。
+
+# 算法：
+# 初始化 ans = 0
+# 从左向右扫描数组：
+#    初始化 max_Left = 0 和 max_right = 0
+#    从当前元素向左扫描并更新：
+#        max_left = max(max_left, height[j])
+#    从当前元素向右扫描并更新：
+#        max_right = max(max_right, height[j])
+#    将min(max_left, max_right) - height[i] 累加到 ans
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        ans = 0
+        size = len(height)
+
+        for i in range(1, size - 1):
+            max_left = max_right = 0
+            for j in range(i, -1, -1):
+                max_left = max(max_left, height[j])
+            for j in range(i, size):
+                max_right = max(max_right, height[j])
+            ans += min(max_left, max_right) - height[i]
+
+        return ans
+
+
+# 时间复杂度： O(n^2)
+# 空间复杂度： O(1)
+
+# leetcode 超出时间限制
+
+
+# 双指针
 class Solution:
     def trap(self, height: List[int]) -> int:
         left = right = water = 0
