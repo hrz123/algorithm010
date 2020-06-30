64f0f6f31d34ce299070c584eaf3b9bf6fe93b9a
hrz123 <2403076194@qq.com>
2020-06-30 15:36:00 +0800

    NEW: 更新第三周全排列写法

--- Week03/本周作业中等/46. 全排列.py
+++ Week03/本周作业中等/46. 全排列.py
@@ -2,6 +2,92 @@
 from typing import List
 
 
+# 使用 数组 来判断某个位置的元素是否已经使用过
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def dfs(depth, nums, size, path, used, res):
+            # recursion terminator
+            if depth == size:
+                res.append(path[:])
+                return
+
+            # process current level logic
+            for i in range(size):
+                if not used[i]:
+                    used[i] = True
+                    path.append(nums[i])
+
+                    # drill down
+                    dfs(depth + 1, nums, size, path, used, res)
+
+                    # reverse current level status if needed
+                    used[i] = False
+                    path.pop()
+
+        size = len(nums)
+        if size == 0:
+            return []
+
+        used = [False for _ in range(size)]
+        res = []
+        dfs(0, nums, size, [], used, res)
+        return res
+
+
+# 使用 hashset 来判断某个位置的元素是否已经使用过
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def dfs(nums, size, depth, hash_set, path, res):
+            if depth == size:
+                res.append(path[:])
+                return
+
+            for i in range(size):
+                if not nums[i] in hash_set:
+                    hash_set.add(nums[i])
+                    path.append(nums[i])
+
+                    dfs(nums, size, depth + 1, hash_set, path, res)
+                    path.pop()
+                    hash_set.remove(nums[i])
+
+        size = len(nums)
+        if size == 0:
+            return []
+        res = []
+        path = []
+        hash_set = set()
+        dfs(nums, size, 0, hash_set, path, res)
+        return res
+
+
+# 使用 位掩码 来判断某个位置的元素是否已经使用过
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def dfs(nums, size, depth, path, used, res):
+            if depth == size:
+                res.append(path[:])
+                return
+
+            for i in range(size):
+                if ((used >> i) & 1) == 0:
+                    used ^= (1 << i)
+                    path.append(nums[i])
+
+                    dfs(nums, size, depth + 1, path, used, res)
+
+                    used ^= (1 << i)
+                    path.pop()
+
+        size = len(nums)
+        if size == 0:
+            return []
+        state = 0
+        res = []
+        dfs(nums, size, 0, [], state, res)
+        return res
+
+
 # 迭代的写法
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
@@ -16,8 +102,9 @@ class Solution:
 
 
 def main():
-    s = Solution()
-    res = s.permute([1, 2, 3])
+    nums = [1, 2, 3]
+    solution = Solution()
+    res = solution.permute(nums)
     print(res)
 
 
---
+++ Week04/每日一题/剑指 Offer 09. 用两个栈实现队列.py
@@ -0,0 +1,36 @@
+# 剑指 Offer 09. 用两个栈实现队列.py
+
+
+class CQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def appendTail(self, value: int) -> None:
+        self.stack1.append(value)
+
+    def deleteHead(self) -> int:
+        if not self.stack1 and not self.stack2:
+            return -1
+        if self.stack2:
+            return self.stack2.pop()
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+
+# Your CQueue object will be instantiated and called as such:
+# obj = CQueue()
+# obj.appendTail(value)
+# param_2 = obj.deleteHead()
+
+def main():
+    queue = CQueue()
+    queue.appendTail(3)
+    print(queue.deleteHead())
+    print(queue.deleteHead())
+
+
+if __name__ == '__main__':
+    main()
