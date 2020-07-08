f0c681409dabb00de4ffba64845ee16a340006d2
hrz123 <2403076194@qq.com>
2020-07-08 13:22:42 +0800

    NEW: 两数之和使用双指针时，数列要有序，而且原来的索引不重要

--- Week01/1.两数之和.py
+++ Week01/1.两数之和.py
@@ -18,9 +18,31 @@ class Solution:
         return []
 
 
+# 两数之和双指针解法
+# 错误的解法
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         l, r = 0, len(nums) - 1
+#         nums_copy = nums[:]
+#         nums.sort()
+#         res = []
+#         while l < r:
+#             s = nums[l] + nums[r]
+#             if s == target:
+#                 res.append([nums[l], nums[r]])
+#                 l += 1
+#                 while l < r and nums[l] == nums[l - 1]:
+#                     l += 1
+#             elif s < target:
+#                 l += 1
+#             else:
+#                 r -= 1
+#         return [nums_copy.index(i) for i in res[0]] if res else res
+
+
 def main():
     s = Solution()
-    res = s.twoSum([2, 11, 15], 9)
+    res = s.twoSum([3, 2, 4], 6)
     print(res)
 
 
