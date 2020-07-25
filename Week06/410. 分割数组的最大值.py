14c3d552e3b9fe8cf532108af9482d50e7bbbd55
hrz123 <2403076194@qq.com>
2020-07-25 14:14:59 +0800

    NEW: 更新分割数组最大值dp写法

--- Week06/410. 分割数组的最大值.py
+++ Week06/410. 分割数组的最大值.py
@@ -27,6 +27,46 @@ class Solution:
         return left
 
 
+# dp解法
+# 我们可以令 f[i][j] 表示将数组的前 i 个数分割为 j 段所能得到的最大连续子数组和的最小值
+# 在进行状态转移时，我们可以考虑第 j 段的具体范围，
+# 即我们可以枚举 k，其中前 k 个数被分割为 j−1 段，
+# 而第 k+1 到第 i 个数为第 j 段。此时，这 j 段子数组中和的最大值，
+# 就等于 f[k][j−1] 与 sub(k+1, i) 中的较大值，
+# 其中 sub(i,j) 表示数组 nums 中下标落在区间 [i,j] 内的数的和。
+# 由于我们要使得子数组中和的最大值最小，因此可以列出如下的状态转移方程
+# f(i, j) = min ( max ( f(k, j-1), sub(k+1, i))) k 0..i-1
+# 对于状态f(i, j)，由于我们能不能分出空的子数组，因此合法的状态 i >= j
+# 对于不合法的状态，由于我们的目标是求出最小值，因此可以将这些状态全部初始化为一个很大的数
+# 在上述转移方程中，一旦我们尝试从不合法的状态f(k, j-1)进行转移，那么max()将会是一个很大的数，
+# 就不会对最外层的min()产生影响。
+# 初始化
+# 我们需要令f(0, 0)为0
+# 在上述的转移方程中，j=1时，唯一的可能性就是钱i个数被分成了1段
+# 如果枚举k等于0，那么就代表着这种情况；
+# 如k!=0, 对应的状态是不合法的，无法转移，所以我们需要f(0, 0) = 0
+# 返回值
+# f(n, m)
+# 优化空间复杂度
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        n = len(nums)
+        _max = float('inf')
+        f = [[_max] * (m + 1) for _ in range(n + 1)]
+        # 前缀和
+        sub = [0] * (n + 1)
+        for i in range(n):
+            sub[i + 1] = sub[i] + nums[i]
+
+        f[0][0] = 0
+        for i in range(1, n + 1):
+            for j in range(1, min(i, m) + 1):
+                for k in range(i):
+                    f[i][j] = min(f[i][j], max(f[k][j - 1], sub[i] - sub[k]))
+
+        return f[n][m]
+
+
 # 以下为自我练习
 # 二分法
 class Solution:
@@ -52,6 +92,32 @@ class Solution:
             if cur > _max:
                 count += 1
                 cur = num
+        return count
+
+
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        l = r = 0
+        for n in nums:
+            l = max(l, n)
+            r += n
+        while l < r:
+            mid = l + ((r - l) >> 1)
+            count = self._count(nums, mid)
+            if count > m:
+                l = mid + 1
+            else:
+                r = mid
+        return l
+
+    def _count(self, nums, k):
+        count = 1
+        _sum = 0
+        for n in nums:
+            _sum += n
+            if _sum > k:
+                count += 1
+                _sum = n
         return count
 
 
